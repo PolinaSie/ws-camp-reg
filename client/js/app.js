16a589cdafb5f004f00da6a7445470ef5ef10332
Polina <neposed222@gmail.com>
2024-07-02 13:01:37 +0300

    add js pop_up correct

--- client/js/app.js
+++ client/js/app.js
@@ -10,3 +10,106 @@ openPopUp.addEventListener('click', function(e) {
 closePopUp.addEventListener('click', () => {
     popUp.classList.remove('active');
 })
+
+const openPopUp2 = document.getElementById('open_pop_up2');
+const closePopUp2 = document.getElementById('pop_up_close2');
+const popUp2 = document.getElementById('pop_up2');
+
+openPopUp2.addEventListener('click', function(e) {
+    e.preventDefault();
+    popUp2.classList.add('active');
+})
+
+closePopUp2.addEventListener('click', () => {
+    popUp2.classList.remove('active');
+})
+
+const openPopUp3 = document.getElementById('open_pop_up3');
+const closePopUp3 = document.getElementById('pop_up_close3');
+const popUp3 = document.getElementById('pop_up3');
+
+openPopUp3.addEventListener('click', function(e) {
+    e.preventDefault();
+    popUp3.classList.add('active');
+})
+
+closePopUp3.addEventListener('click', () => {
+    popUp3.classList.remove('active');
+})
+
+const openPopUp4 = document.getElementById('open_pop_up4');
+const closePopUp4 = document.getElementById('pop_up_close4');
+const popUp4 = document.getElementById('pop_up4');
+
+openPopUp4.addEventListener('click', function(e) {
+    e.preventDefault();
+    popUp4.classList.add('active');
+})
+
+closePopUp4.addEventListener('click', () => {
+    popUp4.classList.remove('active');
+})
+
+const openPopUp5 = document.getElementById('open_pop_up5');
+const closePopUp5 = document.getElementById('pop_up_close5');
+const popUp5 = document.getElementById('pop_up5');
+
+openPopUp5.addEventListener('click', function(e) {
+    e.preventDefault();
+    popUp5.classList.add('active');
+})
+
+closePopUp5.addEventListener('click', () => {
+    popUp5.classList.remove('active');
+})
+
+const openPopUp6 = document.getElementById('open_pop_up6');
+const closePopUp6 = document.getElementById('pop_up_close6');
+const popUp6 = document.getElementById('pop_up6');
+
+openPopUp6.addEventListener('click', function(e) {
+    e.preventDefault();
+    popUp6.classList.add('active');
+})
+
+closePopUp6.addEventListener('click', () => {
+    popUp6.classList.remove('active');
+})
+
+const openPopUp7 = document.getElementById('open_pop_up7');
+const closePopUp7 = document.getElementById('pop_up_close7');
+const popUp7 = document.getElementById('pop_up7');
+
+openPopUp7.addEventListener('click', function(e) {
+    e.preventDefault();
+    popUp7.classList.add('active');
+})
+
+closePopUp7.addEventListener('click', () => {
+    popUp7.classList.remove('active');
+})
+const openPopUp8 = document.getElementById('open_pop_up8');
+const closePopUp8 = document.getElementById('pop_up_close8');
+const popUp8 = document.getElementById('pop_up8');
+
+openPopUp8.addEventListener('click', function(e) {
+    e.preventDefault();
+    popUp8.classList.add('active');
+})
+
+closePopUp8.addEventListener('click', () => {
+    popUp8.classList.remove('active');
+})
+
+const openPopUp9 = document.getElementById('open_pop_up9');
+const closePopUp9 = document.getElementById('pop_up_close9');
+const popUp9 = document.getElementById('pop_up9');
+
+openPopUp9.addEventListener('click', function(e) {
+    e.preventDefault();
+    popUp9.classList.add('active');
+})
+
+closePopUp9.addEventListener('click', () => {
+    popUp9.classList.remove('active');
+})
