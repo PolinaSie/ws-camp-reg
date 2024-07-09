9584c27a6240993eea3758453db90cf07fe8a8a5
Polina <neposed222@gmail.com>
2024-07-09 09:33:01 +0300

    add js pop_up correct and dynamic card

--- client/js/app.js
+++ client/js/app.js
@@ -1,115 +1,84 @@
-const openPopUp = document.getElementById('open_pop_up');
+
+const openPopUp = document.querySelectorAll('.open_pop_up');
 const closePopUp = document.getElementById('pop_up_close');
 const popUp = document.getElementById('pop_up');
 
-openPopUp.addEventListener('click', function(e) {
+for (let i = 0; i < openPopUp.length; i++) {
+    openPopUp[i].addEventListener('click', function(e) {
     e.preventDefault();
     popUp.classList.add('active');
 })
+}
 
 closePopUp.addEventListener('click', () => {
     popUp.classList.remove('active');
 })
 
-const openPopUp2 = document.getElementById('open_pop_up2');
-const closePopUp2 = document.getElementById('pop_up_close2');
-const popUp2 = document.getElementById('pop_up2');
+const cardData = [
+    {
+        heading:'card 1',
+        body:'this is card body 1'
+    },
+    {
+        heading:'card 2',
+        body:'this is card body 2'
+    },
+    {
+        heading:'card 3',
+        body:'this is card body 3'
+    },
+    {
+        heading:'card 4',
+        body:'this is card body 4'
+    },
+    {
+        heading:'card 5',
+        body:'this is card body 5'
+    },
+    {
+        heading:'card 6',
+        body:'this is card body 6'
+    },
+    {
+        heading:'card 7',
+        body:'this is card body 7'
+    },
+    {
+        heading:'card 8',
+        body:'this is card body 8'
+    },
+    {
+        heading:'card 9',
+        body:'this is card body 9'
+    },
+    {
+        heading:'card 10',
+        body:'this is card body 10'
+    }
+]
 
-openPopUp2.addEventListener('click', function(e) {
-    e.preventDefault();
-    popUp2.classList.add('active');
-})
+const postContainer = document.querySelector('.img_and_name_workshop_container');
 
-closePopUp2.addEventListener('click', () => {
-    popUp2.classList.remove('active');
-})
-
-const openPopUp3 = document.getElementById('open_pop_up3');
-const closePopUp3 = document.getElementById('pop_up_close3');
-const popUp3 = document.getElementById('pop_up3');
-
-openPopUp3.addEventListener('click', function(e) {
-    e.preventDefault();
-    popUp3.classList.add('active');
-})
-
-closePopUp3.addEventListener('click', () => {
-    popUp3.classList.remove('active');
-})
-
-const openPopUp4 = document.getElementById('open_pop_up4');
-const closePopUp4 = document.getElementById('pop_up_close4');
-const popUp4 = document.getElementById('pop_up4');
-
-openPopUp4.addEventListener('click', function(e) {
-    e.preventDefault();
-    popUp4.classList.add('active');
-})
-
-closePopUp4.addEventListener('click', () => {
-    popUp4.classList.remove('active');
-})
-
-const openPopUp5 = document.getElementById('open_pop_up5');
-const closePopUp5 = document.getElementById('pop_up_close5');
-const popUp5 = document.getElementById('pop_up5');
-
-openPopUp5.addEventListener('click', function(e) {
-    e.preventDefault();
-    popUp5.classList.add('active');
-})
-
-closePopUp5.addEventListener('click', () => {
-    popUp5.classList.remove('active');
-})
-
-const openPopUp6 = document.getElementById('open_pop_up6');
-const closePopUp6 = document.getElementById('pop_up_close6');
-const popUp6 = document.getElementById('pop_up6');
-
-openPopUp6.addEventListener('click', function(e) {
-    e.preventDefault();
-    popUp6.classList.add('active');
-})
-
-closePopUp6.addEventListener('click', () => {
-    popUp6.classList.remove('active');
-})
-
-const openPopUp7 = document.getElementById('open_pop_up7');
-const closePopUp7 = document.getElementById('pop_up_close7');
-const popUp7 = document.getElementById('pop_up7');
-
-openPopUp7.addEventListener('click', function(e) {
-    e.preventDefault();
-    popUp7.classList.add('active');
-})
-
-closePopUp7.addEventListener('click', () => {
-    popUp7.classList.remove('active');
-})
-const openPopUp8 = document.getElementById('open_pop_up8');
-const closePopUp8 = document.getElementById('pop_up_close8');
-const popUp8 = document.getElementById('pop_up8');
-
-openPopUp8.addEventListener('click', function(e) {
-    e.preventDefault();
-    popUp8.classList.add('active');
-})
-
-closePopUp8.addEventListener('click', () => {
-    popUp8.classList.remove('active');
-})
-
-const openPopUp9 = document.getElementById('open_pop_up9');
-const closePopUp9 = document.getElementById('pop_up_close9');
-const popUp9 = document.getElementById('pop_up9');
-
-openPopUp9.addEventListener('click', function(e) {
-    e.preventDefault();
-    popUp9.classList.add('active');
-})
-
-closePopUp9.addEventListener('click', () => {
-    popUp9.classList.remove('active');
-})
+const postMethods = ()=>{
+    cardData.map((postData)=>{
+        const postElement = document.createElement('div');
+        postElement.classList.add('box_composition');
+        postElement.innerHTML = `
+        <div class="pop_up" id="pop_up">
+                <div class="pop_up_container">
+                    <div class="pop_up_body" id="pop_up_body">
+                        <p>Зареєструватись</p>
+                        <form action="">
+                            <input type="text" placeholder="Ім'я">
+                            <input type="text" placeholder="Телефон">
+                            <button>Надіслати</button>
+                        </form>
+                        <div class="pop_up_close" id="pop_up_close">&#10005</div>
+                    </div>
+                </div>
+            </div>
+            `
+            postContainer.appendChild(postElement)
+    })
+}
+postMethods()
